46603b3593dccf571e2dd2205d08a09e4a42d253
sskibin22 <50159952+sskibin22@users.noreply.github.com>
2025-12-22 17:43:03 -0500

    Add migration for image storage key in collections

---
+++ backend/Migrations/20251222221547_AddImageStorageKeyToCollections.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeManager.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageStorageKeyToCollections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
